56481b4e9e6852688d6e4bc349c19ad885c76b04
bgodeogl <bgodeogl@ford.com.tr>
2024-06-09 21:42:16 +0000

    observer is now being initialized with std::function instead of inheritance

--- include/observer.hpp
+++ include/observer.hpp
@@ -1,23 +1,28 @@
 #ifndef OBSERVER_H_
 #define OBSERVER_H_
 
+#include <functional>
+
 template <typename T>
 class Observable;
 
 template <typename T>
 class Observer {
 public:
-    using Ptr = Observer*;
+    using Handle = std::function<void(const T&)>;
     //TODO: fix raw pointer usage
-    explicit Observer(Observable<T>* observable){
+    explicit Observer(Observable<T>* observable, Handle handle): handle_(handle) {
         observable->Subscribe(this);
     };
     virtual ~Observer() = default;
 
-    virtual void Update(const T& value) = 0;
+    void Update(const T& value){
+        handle_(value);
+    };
 
 private:
     Observable<T>* observable_;
+    std::function<void(T)> handle_;
 };
 
 #endif // OBSERVER_H_
